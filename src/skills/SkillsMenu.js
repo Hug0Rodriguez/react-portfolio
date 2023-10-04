cb5c4d8522ebf8a8a6ddd2afec494cbcdd5a39cc
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2023-10-03 23:41:36 -0400

    Skill menu edits

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import className from "classnames";
 import "../styles/skillsMenu.css";
+import skills from "./skillsData";
+import frontendIcon from "../assets/eagle-emblem.png";
+import backendIcon from "../assets/hawk-emblem.png";
 
 export default class SkillsMenu extends Component {
-    render() {
-        return <div>SkillsMenu</div>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeMenuItem: 1,
+        };
     }
-}
+
+    handleMenuItemClick = (menuItem) => {
+        this.setState({
+            activeMenuItem: menuItem,
+        });
+    };
+
+    renderContent(skills) {
+        return skills.map((skill, index) => (
+            <div
+                key={index}
+                className={`skill-sub-container-${this.state.activeMenuIten}`}
+            >
+                {[...Array(6)].map((_,i) => (
+                    <div
+                        key={i}
+                        className={`level-point ${i < skill.level ? "filled" : "unfilled"}`}
+                    />
+                ))}
+            </div>
+        ))
+    }
---
+++ src/skills/skillsData.js
@@ -0,0 +1,70 @@
+const skills = {
+    1: [
+        {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "CSS",
+            level: 5,
+        },
+        {
+            title: "SASS",
+            level: 2,
+        },
+        {
+            title: "JavaScript",
+            level: 6,
+        },
+        {
+            title: "TypeScript",
+            level: 2,
+        },
+        {
+            title: "Tailwind css",
+            level: 3,
+        },
+        {
+            title: "React JS",
+            level: 6,
+        },
+        {
+            title: "Figme",
+            level: 1,
+        },
+    ],
+    2: [
+        {
+            title: "PostgreSQL",
+            level: 2,
+        },
+        {
+            title: "Node.js",
+            level: 4,
+        },
+        {
+            title: "MonogDB",
+            level: 2,
+        },
+        {
+            title: "GIT",
+            level: 3,
+        },
+        {
+            title: "Python",
+            level: 6,
+        },
+        {
+            title: "FastAPI",
+            level: 4,
+        },
+        {
+            title: "Django",
+            level: 6,
+        },
+        {
+            title: "Express.JS",
+            level: 1,
+        },
+    ],
+};

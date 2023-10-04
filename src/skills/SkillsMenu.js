04234a9002f58d3e59f3fd151e18f680ff87f465
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2023-10-04 13:29:39 -0400

    Completed SkillsMenu

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import className from "classnames";
+import classNames from "classnames";
 import "../styles/skillsMenu.css";
 import skills from "./skillsData";
 import frontendIcon from "../assets/eagle-emblem.png";
@@ -19,18 +19,55 @@ export default class SkillsMenu extends Component {
         });
     };
 
-    renderContent(skills) {
+    renderContent = (skills) => {
         return skills.map((skill, index) => (
             <div
                 key={index}
-                className={`skill-sub-container-${this.state.activeMenuIten}`}
+                className={`skill-sub-container-${this.state.activeMenuItem}`}
             >
-                {[...Array(6)].map((_,i) => (
-                    <div
-                        key={i}
-                        className={`level-point ${i < skill.level ? "filled" : "unfilled"}`}
-                    />
-                ))}
+                <h3>{skill.title}</h3>
+                <div className="level-container">
+                    {[...Array(6)].map((_, i) => (
+                        <div
+                            key={i}
+                            className={`level-point ${
+                                i < skill.level ? "filled" : "unfilled"
+                            }`}
+                        />
+                    ))}
+                </div>
             </div>
-        ))
+        ));
+    };
+
+    render() {
+        const { activeMenuItem } = this.state;
+        const menuItems = ["FRONT-END", "BACK-END"];
+
+        const currentIcon = activeMenuItem === 1 ? frontendIcon : backendIcon;
+
+        return (
+            <div className="skill-menu">
+                {menuItems.map((item, index) => (
+                    <div
+                        key={index}
+                        className={classNames("skill-item", {
+                            activeSkill: activeMenuItem === index + 1,
+                        })}
+                        onClick={() => this.handleMenuItemClick(index + 1)}
+                    >
+                        <h2 className="skill-title">{item}</h2>
+                    </div>
+                ))}
+                <img
+                    className="skill-icon"
+                    src={currentIcon}
+                    alt="current skill"
+                />
+                <div className="skill-sub-container">
+                    {this.renderContent(skills[activeMenuItem])}
+                </div>
+            </div>
+        );
     }
+}
